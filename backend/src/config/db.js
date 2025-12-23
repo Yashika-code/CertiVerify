4ba202f9d07488bd4644693ffeac286438defdf4
Yashika <your-email@example.com>
2025-12-23 15:28:23 +0530

    Renamed project to CertiVerify and updated changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,15 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`\n MongoDB connected !! `)
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected ✅");
     } catch (error) {
-        console.log("MongoDB connection Failed", error);
+        console.error("MongoDB connection failed ❌", error);
         process.exit(1);
     }
-}
+};
 
 export default connectDB;
